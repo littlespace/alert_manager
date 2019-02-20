2e6964f5307b350f4d90279c5c6d041d6f43344b
Mayuresh Gaitonde <mgaitonde@sm-mgaitonde1.local>
2019-02-20 14:38:11 -0800

    revert obs parser

--- listener/parsers/parser_observium.go
+++ listener/parsers/parser_observium.go
@@ -48,7 +48,7 @@ func (p *ObserviumParser) Parse(data []byte) (*listener.WebHookAlertData, error)
 	l := &listener.WebHookAlertData{
 		Id:      d.Id,
 		Name:    d.Message,
-		Details: d.EntityType + " / " + d.EntityName + " / " + d.EntityDescription + "\n" + fmt.Sprintf("Url: %s", d.Url),
+		Details: d.Title + fmt.Sprintf(" Url: %s", d.Url),
 		Device:  d.DeviceName,
 		Entity:  d.EntityName,
 		Time:    t,
