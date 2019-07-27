3f365185f8b23474612ccd24772e148d4ea5d891
Praveen Ponakanti <pponakanti@roblox.com>
2019-07-26 17:38:13 -0700

    Aggregating fibercut alert messages where a-side matches z-side

--- plugins/processors/aggregator/groupers/fibercut.go
+++ plugins/processors/aggregator/groupers/fibercut.go
@@ -34,11 +34,37 @@ func (g fibercutGrouper) Name() string {
 }
 
 func (g fibercutGrouper) AggDesc(alerts []*models.Alert) string {
+
+	type AlertInfo struct {
+		provider, cktId, aSideDev, aSideInt, zSideDev, zSideInt string
+	}
+	var s []AlertInfo
+
 	msg := "Affected Interfaces:\n"
+	is_dup := false
 	for _, a := range alerts {
+
+		is_dup = false
+		for i, entry := range s {
+			if entry.provider == a.Labels["provider"] && entry.cktId == a.Labels["cktId"] && entry.aSideDev == a.Labels["zSideDeviceName"] && entry.aSideInt == a.Labels["zSideInterface"] && entry.zSideDev == a.Labels["aSideDeviceName"] && entry.zSideInt == a.Labels["aSideInterface"] {
+				// Skip printing the alert if this is a duplicate where a/z-Side matches the z/a-Side of cache entry. Remove entry from the local cache.
+				is_dup = true
+				s[i], s[len(s)-1] = s[len(s)-1], s[i]
+				s = s[:len(s)-1]
+				break
+			}
+		}
+		if is_dup == true {
+			continue
+		}
+
+		// If not a duplicate cache this alert for possible dupes and then print the alert info.
+		s = append(s, AlertInfo{a.Labels["provider"].(string), a.Labels["cktId"].(string), a.Labels["aSideDeviceName"].(string), a.Labels["aSideInterface"].(string),
+			a.Labels["zSideDeviceName"].(string), a.Labels["zSideInterface"].(string)})
+
 		msg += fmt.Sprintf(
-			"%s:%s, Provider: %s, CktId: %s\n",
-			a.Device.String, a.Entity, a.Labels["provider"].(string), a.Labels["cktId"].(string),
+			"Provider: %s, CktId: %s, A-Side: %s:%s, Z-Side: %s:%s\n",
+			a.Labels["provider"].(string), a.Labels["cktId"].(string), a.Device.String, a.Entity, a.Labels["zSideDeviceName"].(string), a.Labels["zSideInterface"].(string),
 		)
 	}
 	return msg
